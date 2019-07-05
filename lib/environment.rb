ab3bd455070508367813f5a1d47dae7d3734e2a8
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-05 22:44:11 +0000

    initial commit

---
+++ lib/environment.rb
@@ -0,0 +1,6 @@
+require "us_presidents/version"
+
+module UsPresidents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/us_presidents/version.rb
@@ -0,0 +1,3 @@
+module UsPresidents
+  VERSION = "0.1.0"
+end
