2218c22653849c54684d43e565e9ae278c6f4f19
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-05 23:32:21 +0000

    connect scraper class with cli

--- lib/us_presidents/cli.rb
+++ lib/us_presidents/cli.rb
@@ -1,7 +1,37 @@
 class UsPresidents::CLI 
   
   def start 
-    puts "It's working!"
+   greeting
+   list_presidents
+   user_choice
+   details
+   goodbye
+  end
+  
+  
+  def greeting
+    puts "\nWelcome! Please see a list of US Presidents.\n"
+    puts "\nPlease type a number associated with a president you would like to learn more.\n"
+    
+  end
+  
+  def list_presidents 
+    url = "https://millercenter.org/president"
+    UsPresidents::Scraper.scrape_presidents(url)
+    #scrape names with index
+  
+  end
+  
+  def user_choice 
+    #if statement
+  end
+  
+  def details 
+    #display the information about the president the user selected
+  end
+  
+  def goodbye 
+    puts "Goodbye!"
   end
   
 end
---
+++ lib/us_presidents/scraper.rb
@@ -0,0 +1,9 @@
+class UsPresidents::Scraper 
+  
+  def self.scrape_presidents(url)
+
+  puts "In the scraper presidents class"
+  end  
+  
+  
+end
