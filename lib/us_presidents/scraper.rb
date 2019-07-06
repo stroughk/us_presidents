a1fc970287b4104200b126f557b8608c5795abce
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-06 16:08:39 +0000

    scrape attributres

--- lib/us_presidents/scraper.rb
+++ lib/us_presidents/scraper.rb
@@ -12,4 +12,18 @@ class UsPresidents::Scraper
     url = webpage.search(".field-content a").first.attr("href")
     puts url
   end
+  
+  def self.details 
+    site = "https://millercenter.org/president"  
+    webpage = Nokogiri::HTML(open(site))
+    
+    birth_date = webpage.css(".data.date-data").css("time").first.text
+    death_date = webpage.css(".data.date-data").css("time")[1].text
+    religion = webpage.css(".data.string-data").css("div")[5].text
+    political_party = webpage.css(".data.string-data").css("div")[9].text
+    nickname = webpage.css(".data.string-data").css("div")[11].text
+  end
+    
+  
+  
 end
