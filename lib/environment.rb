b57a8fc8e71d8be95cb0e95c29352510ed89c670
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-07 22:00:02 +0000

    add colorize

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,7 @@
 require "nokogiri"
 require "open-uri"
 require "pry"
+require "colorize"
 
 require_relative "us_presidents/version"
 require_relative "us_presidents/cli"
--- lib/us_presidents/cli.rb
+++ lib/us_presidents/cli.rb
@@ -1,8 +1,8 @@
 class UsPresidents::CLI 
-  
+
   def call 
-    puts "\nWelcome!!!!\n"
-    puts "\nHere is a list of all US Presidents: \n"
+    puts "\nWelcome!!!!\n".yellow
+    puts "\nHere is a list of all US Presidents: \n".blue
     puts 
     UsPresidents::Scraper.new.scrape_articles
     list_stories
@@ -18,19 +18,19 @@ class UsPresidents::CLI
   def menu 
     input = ""
     while input != "exit"
-      puts "\nWhich president do you want to read more information on?\n"
+      puts "\nWhich president do you want to read more information on?\n".blue
       input = gets.strip 
       
       if input.to_i-1 <= UsPresidents::Story.all.size 
         story = UsPresidents::Story.all[input.to_i-1]
         
         
-        puts "\nHere is some quick information: \n"
+        puts "\nHere is some quick information: \n".yellow
         puts "\nPresident's Name: #{story.name}"
         puts "Inaguration: #{story.inaguration}"
         puts "End of presidency: #{story.end_date}"
         
-        puts "Would you like to read more?"
+        puts "Would you like to read more?".yellow
         answer = gets.strip 
         
         if ["Y", "YES"].include?(answer.upcase)
@@ -38,15 +38,16 @@ class UsPresidents::CLI
         puts content
         end 
       end
-      puts "Would you like to exit or list again?"
+      puts "Would you like to exit or list again?".blue
       input = gets.strip 
       
       if input == "list"
+        puts "Here is the list: ".yellow
         list_stories
       elsif input == "exit"  
-        puts "Goodbye!"
+        puts "Goodbye!".yellow
       else
-        puts "Invalid entry"
+        puts "Invalid entry".red
       end
   
     end 
